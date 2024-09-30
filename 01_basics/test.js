bf40528deb243b7138dd800009efa67bad23ea55
itaintlene <105277093+itaintlene@users.noreply.github.com>
2024-09-30 04:55:59 +0000

    container config done

---
+++ 01_basics/test.js
@@ -0,0 +1,4 @@
+console.log("Harleen") // to print on terminal type "$ node 'name of folder>file name'" and press enter :)
+
+// also whatever youre saving here is not getting saved to your git so you have to fork it to your account.
+//
